3847443827554745ec458bba9b413fb614b8b0b0
SerhiiKI13 <kugai.sirozha@gmail.com>
2025-07-15 20:40:39 +0200

    completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.ts";
+import type {IUsers} from "./models/IUser.ts";
 
 function App() {
-
-
+const users = useFetch<IUsers>('https://jsonplaceholder.typicode.com/users');
   return (
-    <>
-
-    </>
+    <div>
+      {users.map((user) => <div key={user.id}>{user.id} && {user.username}</div>)}
+    </div>
   )
 }
 
---
+++ src/service/service.api.ts
@@ -0,0 +1,3 @@
+export const serviceApi =  async <T>(url: string): Promise<T[]> => {
+    return fetch(url).then(response => response.json())
+}
